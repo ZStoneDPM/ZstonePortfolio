8c36cba7d96a1a75c089b7ee709bc87dcc029867
zstone digital project management <zsdpm@zstones-MacBook-Pro.local>
2019-08-23 15:54:57 -0700

    added seeds data and db setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_185734) do
+ActiveRecord::Schema.define(version: 2019_08_23_190423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
